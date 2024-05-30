558380607f735ed7a3c045068f30279e65544a4f
Gwannon <monclus.jorge@gmail.com>
2024-05-30 15:50:17 +0200

    Reordenamos aleatoriamente todas las palabras

--- mimimi/mimimi.php
+++ mimimi/mimimi.php
@@ -11,22 +11,32 @@ $actual_link = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP
 
 $dom = HtmlDomParser::file_get_html($actual_link);
 
-$elems = $dom->find("p,li,h1,h2,h3,h4,h5,td,th,blockquote,a,button");
-$rep = array_rand(array_flip(["a", "e", "i", "o", "u"]));
-foreach ($elems as $elem) {
-  $string = $elem->innertext;
-  $control = 0;
-  $newstring = "";
-  foreach(mb_str_split($string) as $char) {
-    if($char == '<') $control = 1;
-    else if($char == '>') $control = 0;
-    if($control == 0) {
-      $char = preg_replace("/[aeoiuàèìòùáéíóúâêîôûãõäëïöü]/", $rep, $char);
-      $char = preg_replace("/[AEIOUÀÈÌÒÙÁÉÍÓÚÂÊÎÔÛÃÕÄËÏÖÜ]/", strtoupper($rep), $char);
-      $newstring .= utf8_encode($char);
-    } else $newstring .= $char;
+$elems = $dom->find("p,li,h1,h2,h3,h4,h5,td,th,blockquote,button");
+$rand = rand(1,6);
+if($rand >= 1 && $rand <= 5) { //Sustituimos todas las vocales por una vocal al azar
+  $rep = array_rand(array_flip(["a", "e", "i", "o", "u"]));
+  foreach ($elems as $elem) {
+    $string = $elem->innertext;
+    $control = 0;
+    $newstring = "";
+    foreach(mb_str_split($string) as $char) {
+      if($char == '<') $control = 1;
+      else if($char == '>') $control = 0;
+      if($control == 0) {
+        $char = preg_replace("/[aeoiuàèìòùáéíóúâêîôûãõäëïöü]/", $rep, $char);
+        $char = preg_replace("/[AEIOUÀÈÌÒÙÁÉÍÓÚÂÊÎÔÛÃÕÄËÏÖÜ]/", strtoupper($rep), $char);
+        $newstring .= utf8_encode($char);
+      } else $newstring .= $char;
+    }
+    $elem->innertext = $newstring;
   }
-  $elem->innertext = $newstring;
+} else if($rand == 6) { //Reordenamos aleatoriamente todas las palabras
+  foreach ($elems as $elem) {
+    $words = explode( " ", strip_tags($elem->innertext));
+    shuffle($words);
+    $text = mb_strtolower(implode(" ",$words));
+    $elem->innertext = $text;
+  }
 }
 
 $str = $dom->save();
