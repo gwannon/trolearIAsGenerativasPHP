7b2e0939b5c8840965836dc8394c9b9131e63b37
Gwannon <monclus.jorge@gmail.com>
2024-05-31 09:52:57 +0200

    Ahora las imagenes son negativizada, realzadas o puestas en blanco y negro al azar

--- mimimi/mimimimigi.php
+++ mimimi/mimimimigi.php
@@ -14,9 +14,14 @@ if($size['mime'] == 'image/jpeg') {
   $image = imagecreatefromgif($actual_link);
 }
 
+//Giramos la imagen
 imageflip($image, IMG_FLIP_BOTH);
 
-imagefilter($image, IMG_FILTER_NEGATE);
+//Ponemos en negativos/engris/realzamos
+$rand = rand(1,3);
+if($rand == 1) imagefilter($image, IMG_FILTER_NEGATE);
+else if($rand == 2) imagefilter($image, IMG_FILTER_GRAYSCALE);
+else if($rand == 3) imagefilter($image, IMG_FILTER_EMBOSS);
 
 $height = $size[1];
 $width = $size[0];
@@ -24,22 +29,15 @@ $pixelate_y = 5;
 $pixelate_x = 5;
 
 // start from the top-left pixel and keep looping until we have the desired effect
-for($y = 0;$y < $height;$y += $pixelate_y+1)
-{
-
-    for($x = 0;$x < $width;$x += $pixelate_x+1)
-    {
-        // get the color for current pixel
-        $rgb = imagecolorsforindex($image, imagecolorat($image, $x, $y));
-
-        // get the closest color from palette
-        $color = imagecolorclosest($image, $rgb['red'], $rgb['green'], $rgb['blue']);
-        imagefilledrectangle($image, $x, $y, $x+$pixelate_x, $y+$pixelate_y, $color);
-
-    }       
+for($y = 0;$y < $height;$y += $pixelate_y+1) {
+  for($x = 0;$x < $width;$x += $pixelate_x+1) {
+    // get the color for current pixel
+    $rgb = imagecolorsforindex($image, imagecolorat($image, $x, $y));
+    // get the closest color from palette
+    $color = imagecolorclosest($image, $rgb['red'], $rgb['green'], $rgb['blue']);
+    imagefilledrectangle($image, $x, $y, $x+$pixelate_x, $y+$pixelate_y, $color);
+  }       
 }
-
-
 
 if($size['mime'] == 'image/jpeg') imagejpeg($image);
 else if($size['mime'] == 'image/png') imagepng($image);
