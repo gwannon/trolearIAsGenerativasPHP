04acb55617e3ffcf777e69ff9e524ab126821b72
Gwannon <monclus.jorge@gmail.com>
2024-10-14 13:28:06 +0200

    El sxcript cambia los colores aleatorimente en el css

--- mimimi/mimimicss.php
+++ mimimi/mimimicss.php
@@ -17,5 +17,16 @@ $dom = HtmlDomParser::file_get_html($actual_link);
 
 
 $str = $dom->save();
+
+header("Content-Type: text/css");
+
+$res = preg_match_all("/#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})/", $str, $matches);
+
+$matches = array_map('array_unique', $matches);
+
+foreach($matches[0] as $match) {
+  $str = str_replace($match, '#' . str_pad(dechex(mt_rand(0, 0xFFFFFF)), 6, '0', STR_PAD_LEFT), $str);
+}
+
 echo $str;
 echo "#Trolled!!!!";
